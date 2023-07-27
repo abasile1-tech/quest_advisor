308fd24cad763c97ee45ee83cfc4a0ca8d40672c
John <ashton.basile@gmail.com>
2023-07-27 10:22:43 +0100

    I added the gitignore and initialized the database

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+PASSWORD = os.getenv('PASSWORD')
+
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://keithdouglas@localhost:5432/quest_advisor"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:{PASSWORD}@localhost:5432/quest_advisor"
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
